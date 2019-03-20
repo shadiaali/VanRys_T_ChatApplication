5fd5bc6309778fda4ea09a13f7b8104cd684a849
Van Rys <tvanrys@tss-hr420-dmpd.fanshawec.ca>
2019-03-20 13:57:28 -0400

    adding path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
 const port = process.env.PORT || 3000;
 
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.resolve(__dirname + '../views/index.html'));
 });
 
 http.listen(port, () => {
